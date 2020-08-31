ae9789d5d900349d9cd3efc878ff7a2bff865438
Ruan Marcos Gervasi <ruan@ruangervasi.com.br>
2020-08-30 23:03:02 -0300

    GetDetailsFromGitApi

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container } from './styles';
 import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
 export default function Repositorio({match}) {
+
+        useEffect(() => {
+                async function load(){
+                        const nomeRepo = decodeURIComponent(match.params.repositorio);
+
+                        const [repositorioData, issuesData] = await Promise.all([
+                                api.get(`/repos/${nomeRepo}`),
+                                api.get(`/repos/${nomeRepo}/issues`)
+                        ])
+
+                        console.log(repositorioData);
+                        console.log(issuesData);
+                }
+                load();
+        }, []);
+
+        
+
         return (
                 <Container>
                         Repositorio: <span>{decodeURIComponent(match.params.repositorio)}</span>
-                        
                 </Container>
         )
 }
--- src/pages/Repositorio/styles.js
+++ src/pages/Repositorio/styles.js
@@ -1,4 +1,4 @@
-import styled, {keyframes, css} from 'styled-components';
+import styled from 'styled-components';
 
 export const Container = styled.h1`
     max-width: 700px;
